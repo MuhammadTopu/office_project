5bf961aaf972833314c9dbb81d349188830d9cad
Kamran Hossain Topu <117673483+MuhammadTopu@users.noreply.github.com>
2025-03-20 20:39:32 +0600

    feat: update CORS configuration to allow requests from Vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://task-management-git-main-kamran-hossain-topus-projects.vercel.app',
+    ],
     allowedHeaders: ['Content-Type', 'Authorization', 'id'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
